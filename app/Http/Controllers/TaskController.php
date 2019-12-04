6689f6c9d1058d111bd1bf9718e14b6ba4985792
JiaXing <s3a632055@gm.student.ncut.edu.tw>
2019-12-04 21:20:13 +0800

    測試errors.blade.php是否成功

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -46,9 +46,19 @@ class TaskController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
+    /**
+     * 建立新的任務。
+     *
+     * @param  Request  $request
+     * @return Response
+     */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'name' => 'required|max:255',
+        ]);
+
+        // Create The Task...
     }
 
     /**
