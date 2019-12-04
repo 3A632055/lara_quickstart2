255cd0f91a662a070bbf716411dcb4629c88e28f
JiaXing <s3a632055@gm.student.ncut.edu.tw>
2019-12-05 03:02:14 +0800

    增加一個 destroy 方法到 TaskPolicy

--- app/Policies/TaskPolicy.php
+++ app/Policies/TaskPolicy.php
@@ -18,4 +18,15 @@ class TaskPolicy
     {
         //
     }
+    /**
+     * 判斷當給定的使用者可以刪除給定的任務。
+     *
+     * @param  User  $user
+     * @param  Task  $task
+     * @return bool
+     */
+    public function destroy(User $user, Task $task)
+    {
+        return $user->id === $task->user_id;
+    }
 }
